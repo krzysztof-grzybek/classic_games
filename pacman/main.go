f64a90e32d131272ac57fd078de6612f2d8eb708
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-11-02 18:49:40 +0100

    pacman l02

--- pacman/main.go
+++ pacman/main.go
@@ -2,7 +2,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"bufio"
+	"log"
 )
 
 var maze []string
@@ -25,12 +27,58 @@ func loadMaze() error {
 }
 
 func printScreen() {
+	clearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
 
+func init() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+	    log.Fatalf("Cannot enable cbreak mode in terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to enable cooked terminal: %v\n", err)
+	}
+}
+
+func readinput() (string, error) {
+	buffer := make([]byte, 100)
+	cnt, err := os.Stdin.Read(buffer)
+
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row, col)
+}
+
 func main() {
+	defer cleanup()
 
 	err := loadMaze()
     if err != nil {
@@ -38,6 +86,15 @@ func main() {
 	}
 	for {
 		printScreen()
-	    break;
+
+		input, err := readinput()
+		if err != nil {
+			log.Fatalf("Error reading input: %v\n", err)
+			break
+		}
+
+		if input == "ESC" {
+			break
+		}
 	}
 }
