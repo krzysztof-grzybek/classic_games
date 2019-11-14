90cae7eb4ebce844d14320b9306c0ebf97e37e28
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-11-14 20:17:02 +0100

    snake basics

--- snake/main.go
+++ snake/main.go
@@ -1,22 +1,65 @@
 package main
 
 import (
+	"image/color"
 	"log"
+
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
+
+type Size struct {
+	width  int
+	height int
+}
+
+type Position struct {
+	x int
+	y int
+}
+type Config struct {
+	size      Size
+	fieldSize int
+}
+
+var config Config = Config{
+	size: Size{
+		width:  40,
+		height: 50,
+	},
+	fieldSize: 10,
+}
+
+var snake = []Position{Position{3, 6}}
+
+func prepend(x []Position, y Position) []Position {
+	x = append(x, Position{0, 0})
+	copy(x[1:], x)
+	x[0] = y
+	return x
+}
 
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	snake = prepend(snake, Position{snake[0].x + 1, snake[0].y})
+
+	for _, chunk := range snake {
+		image, _ := ebiten.NewImage(config.fieldSize, config.fieldSize, ebiten.FilterDefault)
+		image.Fill(color.RGBA{0xff, 0, 0, 0xff})
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(chunk.x*config.fieldSize), float64(chunk.y*config.fieldSize))
+		screen.DrawImage(image, op)
+	}
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+	widthInPx := config.size.width * config.fieldSize
+	heightInPx := config.size.height * config.fieldSize
+	if err := ebiten.Run(update, widthInPx, heightInPx, 1, "Ssssnake!"); err != nil {
 		log.Fatal(err)
 	}
 }
-
