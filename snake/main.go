2a0361bdffe6afab0b5e20d075702cc8e93e05f8
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-11-16 12:25:46 +0100

    snake - move

--- snake/main.go
+++ snake/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"image/color"
 	"log"
+	"time"
+	"math"
 
 	"github.com/hajimehoshi/ebiten"
 )
@@ -19,6 +21,7 @@ type Position struct {
 type Config struct {
 	size      Size
 	fieldSize int
+	fps int
 }
 
 var config Config = Config{
@@ -27,10 +30,21 @@ var config Config = Config{
 		height: 50,
 	},
 	fieldSize: 10,
+	fps: 6,
+}
+type Snake struct {
+	body []Position
 }
 
-var snake = []Position{Position{3, 6}}
+var snake = newSnake()
+var lastFrameTime time.Time
 
+
+func newSnake() Snake {
+	middleX := int(math.Ceil(float64(config.size.width) / 2))
+	middleY := int(math.Ceil(float64(config.size.height) / 2))
+	return Snake{[]Position{Position{middleX, middleY}, {middleX + 1, middleY},{middleX + 2, middleY}}}
+}
 func prepend(x []Position, y Position) []Position {
 	x = append(x, Position{0, 0})
 	copy(x[1:], x)
@@ -38,20 +52,118 @@ func prepend(x []Position, y Position) []Position {
 	return x
 }
 
-func update(screen *ebiten.Image) error {
-	if ebiten.IsDrawingSkipped() {
-		return nil
+type Direction int
+const (
+	LEFT Direction = iota
+	RIGHT
+	UP
+	DOWN
+)
+var direction = RIGHT
+var lastPressedKey = RIGHT
+var pressed = map[Direction]bool{
+	LEFT: false,
+	RIGHT: false,
+	UP: false,
+	DOWN: false,
+}
+
+func handleKeyPress() {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		if pressed[LEFT] == false && direction != RIGHT {
+			lastPressedKey = LEFT
+		}
+		pressed[LEFT] = true
+	} else {
+		pressed[LEFT] = false
 	}
 
-	snake = prepend(snake, Position{snake[0].x + 1, snake[0].y})
+	if ebiten.IsKeyPressed(ebiten.KeyRight)  {
+		if pressed[RIGHT] == false && direction != LEFT {
+			lastPressedKey = RIGHT
+		}
+		pressed[RIGHT] = true
+	} else {
+		pressed[RIGHT] = false
+	}
 
-	for _, chunk := range snake {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		if pressed[UP] == false && direction != DOWN {
+			lastPressedKey = UP
+		}
+		pressed[UP] = true
+	} else {
+		pressed[UP] = false
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyDown)  {
+		if pressed[DOWN] == false && direction != UP {
+			lastPressedKey = DOWN
+		}
+		pressed[DOWN] = true
+	} else {
+		pressed[DOWN] = false
+	}
+}
+
+func setDirection() {
+	direction = lastPressedKey
+}
+
+func (snake *Snake) move(direction Direction) {
+	if direction == LEFT {
+		snake.body = prepend(snake.body, Position{snake.body[0].x - 1, snake.body[0].y})
+	} else if direction == RIGHT {
+		snake.body = prepend(snake.body, Position{snake.body[0].x + 1, snake.body[0].y})
+	} else if direction == UP {
+		snake.body = prepend(snake.body, Position{snake.body[0].x, snake.body[0].y - 1})
+	} else if direction == DOWN {
+		snake.body = prepend(snake.body, Position{snake.body[0].x, snake.body[0].y + 1})
+	}
+
+	snake.body = snake.body[:len(snake.body) - 1]
+
+	if snake.body[0].x == -1 {
+		snake.body[0].x = config.size.width - 1
+	} else if snake.body[0].x == config.size.width {
+		snake.body[0].x = 0
+	}
+
+	if snake.body[0].y == -1 {
+		snake.body[0].y = config.size.height - 1
+	} else if snake.body[0].y == config.size.height {
+		snake.body[0].y = 0
+	}
+}
+
+func (snake Snake) render(screen *ebiten.Image) {
+	for _, chunk := range snake.body {
 		image, _ := ebiten.NewImage(config.fieldSize, config.fieldSize, ebiten.FilterDefault)
 		image.Fill(color.RGBA{0xff, 0, 0, 0xff})
 		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Translate(float64(chunk.x*config.fieldSize), float64(chunk.y*config.fieldSize))
 		screen.DrawImage(image, op)
 	}
+}
+
+func update(screen *ebiten.Image) error {
+	handleKeyPress()
+
+	if lastFrameTime.IsZero() {
+		lastFrameTime = time.Now()
+		setDirection()
+		snake.move(direction)
+	} else if time.Now().Sub(lastFrameTime).Milliseconds() > int64(1000 / config.fps) {
+		lastFrameTime = time.Now()
+		setDirection()
+		snake.move(direction)
+	}
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	snake.render(screen)
 
 	return nil
 }
